5efd41b44920e67835371fef29650490c3d78223
Victor Rayan <victor.rayan378@gmail.com>
2023-07-17 14:47:09 -0300

    lastupdate hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.hosts << "gces-20231-production.up.railway.app"
 
   # Eager load code on boot.
   config.eager_load = true
